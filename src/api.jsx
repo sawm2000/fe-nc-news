21e436103ba83e82a8d9a8681d069db785045b82
sawm2000 <sabreenmaflahi@outlook.com>
2024-02-06 11:54:07 +0000

    vote on an article

--- src/api.jsx
+++ src/api.jsx
@@ -4,14 +4,24 @@ const baseApi = axios.create({
     baseURL: "https://nc-news-c0co.onrender.com",
   });
 
-  export const getArticles = () => {
-    return baseApi.get("api/articles").then((response) => {
+  export const getArticles = (article_id) => {
+let endPointString = "api/articles"
+    if(article_id !== undefined){
+            endPointString += `/${article_id}`
+    }
+    return baseApi.get(endPointString).then((response) => {
       return response.data;
     });
   };
 
   export const getComments = (article_id) => {
     return baseApi.get(`/api/articles/${article_id}/comments`).then((response) => {
+      return response.data;
+    });
+  };
+
+  export const patchArticles = (article_id, patchObject) => {
+    return baseApi.patch(`/api/articles/${article_id}`, patchObject).then((response) => {
       return response.data;
     });
   };
