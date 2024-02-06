103870139f67bbcdc6a17427fe26466c660ba3d7
sawm2000 <sabreenmaflahi@outlook.com>
2024-02-06 08:55:02 +0000

    view article comments

---
+++ src/components/CommentCard.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { getComments } from "../api";
+import Carousel from "react-bootstrap/Carousel";
+
+function CommentCard({ article_id , commentCount}) {
+  const [comments, setComments] = useState([]);
+  const [index, setIndex] = useState(1);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
+  useEffect(() => {
+    getComments(article_id).then((response) => {
+      setComments(response.comments);
+    });
+  }, []);
+  return (
+    <>
+    <h3>Comments {commentCount}</h3>
+      <Carousel
+        id="carousel"
+        activeIndex={index}
+        onSelect={handleSelect}
+        variant="dark"
+      >
+        {comments.map((comment) => {
+          return (
+            <Carousel.Item interval={2500} className="comment-list" key={comment.comment_id}>
+              <p>{comment.body}</p>
+              <p>By: {comment.author}</p>
+              <p>Votes: {comment.votes}</p>
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
+    </>
+  );
+}
+
+export default CommentCard;
